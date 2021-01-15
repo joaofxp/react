1a8a7558362f8a6071ca974ef81d1c2de60900d3
joaofxp <joaofxp@hotmail.com>
2021-01-15 13:09:54 -0300

    5 - A milha extra - NLW#1 Booster

--- rocket-seat-next-level-week/server/src/config/multer.ts
+++ rocket-seat-next-level-week/server/src/config/multer.ts
@@ -5,6 +5,12 @@ import crypto from "crypto";
 export default {
     storage: multer.diskStorage({
         destination: path.resolve(__dirname, "..", "..", "uploads"),
-        filename(request, file, callback) {},
+        filename(request, file, callback) {
+            const hash = crypto.randomBytes(6).toString("hex");
+
+            const fileName = `${hash}-${file.originalname}`;
+
+            callback(null, fileName);
+        },
     }),
 };
